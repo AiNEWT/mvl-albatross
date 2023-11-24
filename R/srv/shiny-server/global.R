0383872a9b1fd4ead8362d4a76075f51fe250d37
Donghan Kim <marantz@miningvizlab.com>
2023-11-24 13:24:59 +0900

    RD-77 Shiny Manager Add

--- R/srv/shiny-server/global.R
+++ R/srv/shiny-server/global.R
@@ -1,4 +1,5 @@
 if (!require(shiny)) {install.packages("shiny"); library(shiny)}
+if (!require(shiny)) {install.packages("shinymanager"); library(shinymanager)}
 
 source("./ui.R", local = TRUE)  
 source("./server.R", local = TRUE)  
